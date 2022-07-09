7cc81760433778e74c8882cc399f8ebf27a165c2
bhagyashreeat <bhagyashree@emproto..com>
2022-07-09 12:52:52 +0530

    bug fixes

--- dist/components/TextInput.js
+++ dist/components/TextInput.js
@@ -7,11 +7,13 @@ exports.default = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
+require("./TextInput.css");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-const Button = props => {
+const TextInput = props => {
   return /*#__PURE__*/_react.default.createElement("button", {
-    className: "btn btn--".concat(props.kind, " CTA"),
+    className: "text-input",
     "data-id": props.id,
     type: props.type,
     name: props.name,
@@ -21,5 +23,5 @@ const Button = props => {
   }, /*#__PURE__*/_react.default.createElement("h4", null, props.label));
 };
 
-var _default = Button;
+var _default = TextInput;
 exports.default = _default;
--- dist/index.js
+++ dist/index.js
@@ -3,13 +3,13 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-Object.defineProperty(exports, "Button", {
+Object.defineProperty(exports, "TextInput", {
   enumerable: true,
   get: function get() {
-    return _Button.default;
+    return _TextInput.default;
   }
 });
 
-var _Button = _interopRequireDefault(require("./components/Button"));
+var _TextInput = _interopRequireDefault(require("./components/TextInput"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
